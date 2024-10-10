7baf57ea2213150ea165ef40044fe4f95f0a1eed
SWELIJ <elijahtsheridan25@gmail.com>
2024-10-10 17:19:28 -0400

    made startTime and endTime private

--- stopwatch.js
+++ stopwatch.js
@@ -1,30 +1,34 @@
 class Stopwatch {
   constructor() {
     this.duration = 0
+    this.running = false
   }
   
+   #startTime = {}
+   #endTime = {}
 
   start() {
     this.running = true;
-    this.startTime = new Date();
+    this.#startTime = new Date();
   }
 
   stop() {
     if (!this.running) throw new Error("Must start stopwatch before stopping.");
     this.running = false;
-    this.endTime = new Date();
+    this.#endTime = new Date();
 
-    const seconds = (this.endTime.getTime() - this.startTime.getTime()) / 1000;
+    const seconds = (this.#endTime.getTime() - this.#startTime.getTime()) / 1000;
     this.duration += seconds;
   }
 
   reset() {
     this.duration = 0;
     this.running = false;
-    this.startTime = null;
-    this.endTime = null;
+    this.#startTime = null;
+    this.#endTime = null;
   }
 
 }
 
 const sw = new Stopwatch();
+console.log(sw)
